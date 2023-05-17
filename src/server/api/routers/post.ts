e1f1ba9cee5128c357957c18f5c304f273913b2d
Oscar Fonseca <edsphinx@gmail.com>
2023-05-16 18:03:08 -0600

    Post with enter, toast for error, loading anim for post

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -48,7 +48,7 @@ export const postRouter = createTRPCRouter({
     });
   }),
 
-  create: privateProcedure.input(z.object({ content: z.string().emoji().min(1).max(280) })).mutation(async ({ ctx, input }) => {
+  create: privateProcedure.input(z.object({ content: z.string().emoji("Only emojis are allowed").min(1).max(280) })).mutation(async ({ ctx, input }) => {
     const authorId = ctx.userId;
 
     const { success } = await ratelimit.limit(authorId);
