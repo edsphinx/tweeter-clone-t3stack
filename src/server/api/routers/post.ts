8f15bb2534e8b1fb7a008b0afd80d61689f4222e
Oscar Fonseca <edsphinx@gmail.com>
2023-05-18 13:55:50 -0600

    Profile page with ssg helper

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,15 +1,11 @@
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/server";
 import { TRPCError } from "@trpc/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { z } from "zod";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
-};
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 request per 1 minute
 const ratelimit = new Ratelimit({
